02007824757496d93817c49725bcdcab4b0bbcd7
Sebastian Mertic <alumer2408@ieselcaminas.org>
2026-02-22 21:27:34 +0100

    Proyecto limpio, sin implementar

---
+++ app/src/main/java/com/sebas/gestordenotascortas_geo_sebas/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sebas.gestordenotascortas_geo_sebas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sebas.gestordenotascortas_geo_sebas.ui.theme.GestorDeNotasCortas_Geo_SebasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GestorDeNotasCortas_Geo_SebasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GestorDeNotasCortas_Geo_SebasTheme {
+        Greeting("Android")
+    }
+}
